2240dda84650c8a6d66d668e8f8800e168445384
Carlos <carlosicaraballo1999@gmail.com>
2025-01-30 23:31:35 +0000

    23. Listas en Thymeleaf

--- src/main/java/com/carlos/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/carlos/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -2,9 +2,13 @@ package com.carlos.curso.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.carlos.curso.springboot.webapp.springboot_web.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -15,6 +19,20 @@ public class UserController {
     model.addAttribute("title", "Hola Mundo Spring Boot");
     model.addAttribute("user", user);
     return "details";
+  }
+
+  @GetMapping("list")
+  public String list(ModelMap model) {
+    List<User> users = new ArrayList<>();
+
+    model.addAttribute("users", users);
+    model.addAttribute("title", "Lista de usuarios");
+
+    users.add(new User("Carlos", "Caraballo"));
+    users.add(new User("Xiomara", "Jimenez"));
+    users.add(new User("Pepe", "Santana"));
+
+    return "list";
   }
 
 }
