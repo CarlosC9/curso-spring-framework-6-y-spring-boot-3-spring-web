e7c19a282729d87a19844d07a0c56159bdd64a76
Carlos <carlosicaraballo1999@gmail.com>
2025-01-30 23:50:00 +0000

    25. Anotación @ModelAttribute

--- src/main/java/com/carlos/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/carlos/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.carlos.curso.springboot.webapp.springboot_web.models.User;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,16 +24,17 @@ public class UserController {
 
   @GetMapping("list")
   public String list(ModelMap model) {
-    List<User> users = new ArrayList<>();
-
-    model.addAttribute("users", users);
     model.addAttribute("title", "Lista de usuarios");
+    return "list";
+  }
 
+  @ModelAttribute("users")
+  public List<User> usersModel() {
+    List<User> users = new ArrayList<>();
     users.add(new User("Carlos", "Caraballo"));
     users.add(new User("Xiomara", "Jimenez", "xomi@example.com"));
     users.add(new User("Pepe", "Santana", "pepe@example.com"));
-
-    return "list";
+    return users;
   }
 
 }
