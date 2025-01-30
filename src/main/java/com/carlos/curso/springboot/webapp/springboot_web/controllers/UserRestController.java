ccc3c4b82aa1503707e92f2a4c1a6e1ecefb83db
Carlos <carlosicaraballo1999@gmail.com>
2025-01-30 22:38:15 +0000

    19 Clase DTO

--- src/main/java/com/carlos/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/carlos/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -3,6 +3,7 @@ package com.carlos.curso.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.carlos.curso.springboot.webapp.springboot_web.models.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,16 @@ import com.carlos.curso.springboot.webapp.springboot_web.models.User;
 public class UserRestController {
 
   @GetMapping("/details")
-  public Map<String, Object> details() {
+  public UserDto details() {
+    User user = new User("Carlos", "Caraballo");
+    UserDto userDto = new UserDto();
+    userDto.setUser(user);
+    userDto.setTitle("Hola Mundo Spring Boot");
+    return userDto;
+  }
+
+  @GetMapping("/details-map")
+  public Map<String, Object> detailsMap() {
     User user = new User("Carlos", "Caraballo");
     Map<String, Object> body = new HashMap<>();
 
