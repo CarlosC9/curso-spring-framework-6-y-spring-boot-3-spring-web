965fc53ad3b15c753e1b0b4daae93f625cd9061a
Carlos <carlosicaraballo1999@gmail.com>
2025-01-31 00:32:00 +0000

    28. Objeto request desde HttpServletRequest

--- src/main/java/com/carlos/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
+++ src/main/java/com/carlos/curso/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -1,6 +1,10 @@
 package com.carlos.curso.springboot.webapp.springboot_web.controllers;
 
 import com.carlos.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+import com.carlos.curso.springboot.webapp.springboot_web.models.dto.ParamMixDto;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,5 +23,32 @@ public class RequestParamsController {
 
     return param;
   }
+
+  @GetMapping("/bar")
+  public ParamMixDto bar(
+    @RequestParam String text,
+    @RequestParam Integer code
+  ) {
+    ParamMixDto params = new ParamMixDto();
+    params.setMessage(text);
+    params.setCode(code);
+    return params;
+  }
+
+  @GetMapping("/request")
+  public ParamMixDto request(
+    HttpServletRequest request
+    ) {
+    ParamMixDto params = new ParamMixDto();
+    Integer code = 10;
+    try {
+      code = Integer.parseInt(request.getParameter("code"));
+    } catch (NumberFormatException e) {
+    }
+    params.setCode(code);
+    params.setMessage(request.getParameter("message"));
+    return params;
+  }
+
 
 }
